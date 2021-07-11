64aca1455ac26f8f35ce168578bb7adeb3e7510c
NITISH KUMAR <nkysy713@gmail.com>
2021-07-11 15:33:54 +0530

    adding up ejs files and rendering them

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,8 +1,10 @@
  module.exports.home = function(req,res){
-     return res.end('<h1>Express is up for codeial!</h1>');
+     return res.render('home',{
+         title: "Home"
+     });
  }
 
- module.exports.base = function(req,res){
-    return res.end('<h1>Base!</h1>');
-}
+//  module.exports.base = function(req,res){
+//     return res.end('<h1>Base!</h1>');
+// }
 
--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const homeController = require('../controllers/home_controller');
 
 router.get('/', homeController.home);
-router.get('/base', homeController.base);
+//router.get('/base', homeController.base);
 
 router.use('/users',require('./users'));
 router.use('/posts',require('./post'));
