4b3aec97cfacd4bc1cf7e2eb4c18ec6fb86aee32
NITISH KUMAR <nkysy713@gmail.com>
2021-07-23 01:14:15 +0530

    rendering sign in and sign up pages

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,11 +1,14 @@
+//render sign in page
 module.exports.signin = function(req,res){
-    res.render('sign_in',{
-        title: "Sign In"
+    return res.render('user_sign_in',{
+        title: "Codeial | Sign In"
     });
 }
 
+
+//render sign up page
 module.exports.signup = function(req,res){
-    res.render('sign_up',{
-        title: "Sign Up"
+    return res.render('user_sign_up',{
+        title: "Codeial | Sign Up"
     });
 }
