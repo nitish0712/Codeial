559679e4d923ca6dabbe52ec3055d234293bc29d
NITISH KUMAR <nkysy713@gmail.com>
2021-07-23 01:01:44 +0530

    sign in and sign up forms are added

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,5 +1,11 @@
-module.exports.profile = function(req,res){
-    res.render('users',{
-        title: "Profile"
+module.exports.signin = function(req,res){
+    res.render('sign_in',{
+        title: "Sign In"
+    });
+}
+
+module.exports.signup = function(req,res){
+    res.render('sign_up',{
+        title: "Sign Up"
     });
 }
--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,9 @@ const router = express.Router();
 
 const usersController = require('../controllers/users_controller');
 
-router.get('/profile', usersController.profile);
+router.get('/signin', usersController.signin);
+
+router.get('/signup', usersController.signup);
 
 
 module.exports = router;
