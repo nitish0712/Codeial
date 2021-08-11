daa123a926c78623615e68ecb14d3e26d694a20e
NITISH KUMAR <nkysy713@gmail.com>
2021-08-12 01:35:06 +0530

    Creating Schema for Comments

--- models/post.js
+++ models/post.js
@@ -9,7 +9,14 @@ const postSchema = new mongoose.Schema({
     user:{
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User'
-    }
+    },
+    // include the array of id's of all comments in this post itself
+    comments: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'comment'
+        }
+    ]
 },{
     timestamps: true
 });
