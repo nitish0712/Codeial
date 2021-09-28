0593808e6d404b0c471b1cdf1880b1155d355777
NITISH KUMAR <nkysy713@gmail.com>
2021-09-28 06:09:55 +0530

    sends mails using template

--- mailer/comments_mailer.js
+++ mailer/comments_mailer.js
@@ -2,13 +2,13 @@ const nodeMailer = require('../config/nodemailer');
 
 
 module.exports.newComment = (comment) => {
-    console.log('inside new comment mailer', comment);
+    let htmlString = nodeMailer.renderTemplate({comment: comment}, '/comments/new_comment.ejs');
 
     nodeMailer.transporter.sendMail({
         from: 'nitishkumar07081999@gmail.com',
         to: comment.user.email,
         subject: "New Comment Published",
-        html: '<h1> YUP, your comment is now published</h1>'
+        html: htmlString
     },(err,info)=>{
         if(err){console.log('error in sending mail', err); return}
 
